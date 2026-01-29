be04a97942ea096b6f8b59e1c46d2159bc3d7084
DominicDev <s0l0estudi4r@gmail.com>
2026-01-29 16:00:27 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'NEON GENESIS EVANGELION';
+export let nameSongs = [
+	"FLY ME TO THE MOON", /*1*/
+	"NEON GENESIS", /*2*/
+	"SPARK", /*3*/
+	"KOMM SÜSSER TOD", /*4*/
+	"A CRUEL ANGEL'S THESIS", /*5*/
+	"INNOCENCE", /*6*/
+	"THE GREAT GIG IN THE SKY", /*7*/
+	"EVA", /*8*/
+	"ONE LAST KISS", /*9*/
+	"BEAUTIFUL WORLD", /*10*/
+	"RUNNING UP THAT HILL", /*11*/
+	"EVA 2", /*12*/
+	"MY WAY", /*13*/
+	"LIBERA ME FROM HELL", /*14*/
+	"桜 流 し", /*15*/
+	"THIS IS THE DREAM", /*16*/
+	" EVERYTHING YOU'VE EVER DREAMED", /*17*/
+	" SAKURA NAGASHI ", /*18*/
+	"BEETHOVEN 9", /*19*/
+	"REI I", /*20*/
+]
